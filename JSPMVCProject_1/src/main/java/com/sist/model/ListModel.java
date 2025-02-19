8f4617a45fa4a2cc43fea9833b1d69b876a76915
sist <sist@sist-101>
2025-02-19 15:46:55 +0900

    1111

--- JSPMVCProject_1/src/main/java/com/sist/model/ListModel.java
+++ JSPMVCProject_1/src/main/java/com/sist/model/ListModel.java
@@ -1,12 +1,22 @@
 package com.sist.model;
 
 import jakarta.servlet.http.HttpServletRequest;
-
+import java.util.*;
+import com.sist.dao.*;
+import com.sist.vo.*;
 public class ListModel implements Model{
 
 	@Override
 	public String handlerRequest(HttpServletRequest request) {
 		// TODO Auto-generated method stub
+		//1. 사용자 요청 => page
+		String page=request.getParameter("page");
+		if(page==null)
+			page="1"; // default page
+		
+		// 현재 페이지 지정 
+		int curpage=Integer.parseInt(page);
+		// 데이터 읽기 
 		
 		return "board/list.jsp";
 	}
